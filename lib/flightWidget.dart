c22de71c80d3f29596ed904f1ba455e3c44dcb85
sashatankov <isasha96@gmail.com>
2020-08-16 15:06:40 +0300

    skeleton for the flight widget is added

---
+++ lib/flightWidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FlightWidget extends StatefulWidget {
+  @override
+  _FlightWidgetState createState() => _FlightWidgetState();
+}
+
+class _FlightWidgetState extends State<FlightWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
