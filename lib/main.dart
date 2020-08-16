308c3a1915ccc30b0a6b5a450ad0ca7cbbe38524
sashatankov <isasha96@gmail.com>
2020-08-16 15:08:10 +0300

    flights label added, colors to icons added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'flightWidget.dart';
 
 void main() {
   runApp(Air9App());
@@ -11,22 +12,15 @@ class Air9App extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
-
       ),
-      home: DefaultTabController(
-        length: 3,
-        child: HomeScreen(title: 'Air9')
-      ),
+      home: DefaultTabController(length: 3, child: HomeScreen(title: 'Air9')),
     );
   }
 }
 
-
 class HomeScreen extends StatefulWidget {
-
   HomeScreen({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -46,49 +40,65 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-
         title: Text(widget.title),
       ),
       body: Center(
-
         child: HomeScreenBody(),
       ),
     );
   }
 }
 
-
-
 class HomeScreenBody extends StatefulWidget {
-
   @override
   _HomeScreenBodyState createState() => _HomeScreenBodyState();
-
 }
 
 class _HomeScreenBodyState extends State<HomeScreenBody> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            child: Text(
+              "Upcoming Flights",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            padding: EdgeInsets.fromLTRB(16, 24, 16, 24),
+          ),
+          // TODO to add upcoming flights widgets
+        ],
+      ),
+      bottomNavigationBar: TabBar(
+        tabs: [
+          Tab(
+            icon: Icon(
+              Icons.local_airport, 
+            color: Colors.blue,
+            semanticLabel: "flights",
+            ),
+          ),
+          Tab(
+            icon: Icon(
+              Icons.search, 
+              color: Colors.blue,
+              semanticLabel: "Search Flights",),
+          ),
+          Tab(
+            icon: Icon(
+              Icons.account_circle, 
+              color: Colors.blue,
+              semanticLabel: "profile",),
+          ),
+        ],
 
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Upcoiming Flights"),
-              // TODO to add upcoming flights widgets
-              ],
-        ),
-        bottomNavigationBar: TabBar(
-          tabs: [
-            Tab(icon: Icons.local_airport,),
-            Tab(icon: Icons.search,), 
-            Tab(icon: Icons.account_circle)
-          ],
-        ),
+      ),
     );
   }
 }
