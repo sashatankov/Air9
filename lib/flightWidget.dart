5a280b22534948f0c91658f19d0ccc33638b91d6
sashatankov <isasha96@gmail.com>
2020-08-18 08:56:28 +0300

    new project commit

---
+++ lib/flightWidget.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'flight.dart';
+
+
+
+class FlightWidget extends StatefulWidget {
+  final Flight flight;
+  FlightWidget(this.flight);
+
+  @override
+  _FlightWidgetState createState() => _FlightWidgetState();
+}
+
+class _FlightWidgetState extends State<FlightWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              this.departureInfo(),
+              Icon(
+                Icons.arrow_forward,
+                color: Colors.black54,
+              ),
+              this.arrivalInfo()
+            ],
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+          ),
+          Container(child: Text("${this.connectionsMessage()}")),
+        ],
+      ),
+      padding: EdgeInsets.all(16),
+    );
+  }
+
+  String connectionsMessage() {
+    var numOfConnections = this.widget.flight.connections.length;
+    if (numOfConnections > 1) {
+      return "${numOfConnections} connections";
+    } else if (numOfConnections == 1) {
+      return "${numOfConnections} connection";
+    }
+    return "";
+  }
+
+  Widget departureInfo() {
+    return this.locationInfoColumn(
+        this.widget.flight.departureCity,
+        this.widget.flight.departureAirport,
+        this.widget.flight.departureDate,
+        this.widget.flight.arrivalTime);
+  }
+
+  Widget arrivalInfo() {
+    return this.locationInfoColumn(
+        this.widget.flight.arrivalCity,
+        this.widget.flight.arrivalAirport,
+        this.widget.flight.arrivalDate,
+        this.widget.flight.arrivalTime);
+  }
+
+  Widget locationInfoColumn(
+      String city, String airport, String date, String time) {
+    return Column(
+      children: [
+        Text("$city", style: this.cityStyle()),
+        Text("$airport", style: this.airportStyle()),
+        Text("$date  $time", style: this.dateStyle()),
+      ],
+    );
+  }
+
+  TextStyle cityStyle() {
+    return TextStyle(color: Colors.black87, fontSize: 24);
+  }
+
+  TextStyle airportStyle() {
+    return TextStyle(color: Colors.black38, fontSize: 16);
+  }
+
+  TextStyle dateStyle() {
+    return TextStyle(color: Colors.black87, fontSize: 16);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,146 @@
+import 'package:flutter/material.dart';
+import 'flightWidget.dart';
+import 'flightData.dart';
+
+void main() {
+  runApp(Air9App());
+}
+
+class Air9App extends StatelessWidget {
+  // This widget is the root of the application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(title: 'Air9'),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: DefaultTabController(length: 3, child: HomeScreenBody()),
+      ),
+    );
+  }
+}
+
+class HomeScreenBody extends StatefulWidget {
+  @override
+  _HomeScreenBodyState createState() => _HomeScreenBodyState();
+}
+
+class _HomeScreenBodyState extends State<HomeScreenBody> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: this.getHomeScreenTabView(),
+      bottomNavigationBar: this.getHomeScreenTabBar(),
+    );
+  }
+
+  List<Widget> getFlightsList() {
+    return randomFlights(10).map((e) => FlightWidget(e)).toList();
+  }
+
+  Widget getFlightsTabView() {
+    var flights = this.getFlightsList();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          child: Text(
+            "Upcoming Flights",
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          padding: EdgeInsets.fromLTRB(16, 24, 16, 24),
+        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: flights.length,
+                itemBuilder: (context, position) {
+                  return Card(child: flights[position]);
+                }))
+      ],
+    );
+  }
+
+  Widget getSearchTabView() {
+    return Container(child: Center(child: Text("Search"))); // TODO
+  }
+
+  Widget getProfileTabView() {
+    return Container(child: Center(child: Text("Profile"))); // TODO
+  }
+
+  Widget getFlightsTab() {
+    return Tab(
+      icon: Icon(
+        Icons.local_airport,
+        color: Colors.blue,
+        semanticLabel: "flights",
+      ),
+    );
+  }
+
+  Widget getSearchTab() {
+    return Tab(
+      icon: Icon(
+        Icons.search,
+        color: Colors.blue,
+        semanticLabel: "Search Flights",
+      ),
+    );
+  }
+
+  Widget getProfileTab() {
+    return Tab(
+      icon: Icon(
+        Icons.account_circle,
+        color: Colors.blue,
+        semanticLabel: "profile",
+      ),
+    );
+  }
+
+  Widget getHomeScreenTabView() {
+    return TabBarView(
+      children: [
+        this.getFlightsTabView(),
+        this.getSearchTabView(),
+        this.getProfileTabView(),
+      ],
+    );
+  }
+
+  Widget getHomeScreenTabBar() {
+    return TabBar(
+      tabs: [
+        this.getFlightsTab(),
+        this.getSearchTab(),
+        this.getProfileTab(),
+      ],
+    );
+  }
+}
