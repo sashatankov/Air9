a7b20054b10f85d5d30c12fc3b0205ee284a8833
sashatankov <isasha96@gmail.com>
2020-08-24 17:15:35 +0300

    app controller added

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,24 @@ import 'package:flutter/material.dart';
 import 'home_screen.dart';
 
 void main() {
-  runApp(Air9App());
+  AppController appController = AppController();
+  runApp(Air9App(appController));
+}
+
+class AppController {
+  HomeScreenController homeScreenController;
+
+  AppController() {
+    this.homeScreenController = HomeScreenController();
+  }
 }
 
 class Air9App extends StatelessWidget {
+  AppController appController;
+
+  Air9App(AppController controller) {
+    this.appController = controller;
+  }
   // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
@@ -16,12 +30,11 @@ class Air9App extends StatelessWidget {
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: HomeScreen(title: 'Air9'),
+        home: HomeScreen(this.appController.homeScreenController, title: 'Air9'),
       ),
       onTap: () {
         FocusScopeNode currentFocus = FocusScope.of(context);
         currentFocus.requestFocus(new FocusNode());
-        
       },
     );
   }
