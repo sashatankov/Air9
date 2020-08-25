cc6d27ed9574aa20913d42806ae2ce9281f7c533
sashatankov <isasha96@gmail.com>
2020-08-25 09:35:49 +0300

    test cases now have controller as adependency of the app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,8 +13,8 @@ import 'package:Air9/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(Air9App());
-
+    await tester.pumpWidget(Air9App(AppController()));
+  
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
