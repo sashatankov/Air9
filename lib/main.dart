b30370341bc573b8f6337107f967d7ba6f1e8c1f
sashatankov <isasha96@gmail.com>
2020-08-26 10:02:02 +0300

    app conroller added. code added in main() to init the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,37 @@
+import 'package:Air9/src/account.dart';
+import 'package:Air9/src/account_data.dart';
+import 'package:Air9/src/flight.dart';
+import 'package:Air9/src/flight_data.dart';
+import 'package:Air9/src/flight_search.dart';
 import 'package:flutter/material.dart';
 import 'package:Air9/src/home_screen.dart';
 
 void main() {
-  AppController appController = AppController();
+  var flights = randomFlights(10);
+  Flights flightsModel = Flights(flights);
+  FlightsController flightsCotroller = FlightsController(flightsModel);
+
+  TravelerAccount account = randomTravelerAccount();
+  TravelerAccountController accountcontroller =
+      TravelerAccountController(account);
+
+  FlightSearchController flightSearchController = FlightSearchController();
+
+  HomeScreenModel homeScreenModel = HomeScreenModel(
+    flightsCotroller, 
+    accountcontroller, 
+    flightSearchController);
+  HomeScreenController homeScreenController =
+      HomeScreenController(homeScreenModel);
+      
+  AppController appController = AppController(homeScreenController);
+
   runApp(Air9App(appController));
 }
 
 class AppController {
   HomeScreenController homeScreenController;
-
-  AppController() {
-    this.homeScreenController = HomeScreenController();
-  }
+  AppController(this.homeScreenController);
 }
 
 class Air9App extends StatelessWidget {
