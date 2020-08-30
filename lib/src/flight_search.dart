c241dac2b100502829ffd9f7029d41daf610acf0
sashatankov <isasha96@gmail.com>
2020-08-30 11:19:37 +0300

    documentation added to the flight_search module

--- lib/src/flight_search.dart
+++ lib/src/flight_search.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+/// a class represents a search query in the search screen in the app
 class FlightSearchQuery {
   String from = "";
   String to = "";
@@ -11,45 +12,63 @@ class FlightSearchQuery {
   FlightSearchQuery();
 }
 
+/// a controller class for the interaction with [FlightSearchQuery] model
 class FlightSearchController {
   FlightSearchQuery model;
   FlightSearchView flightSearchView;
 
+  /// a constructor for the class, with empty FlightSearchQuery model
   FlightSearchController() {
     this.model = FlightSearchQuery();
     this.flightSearchView = FlightSearchView(this);
   }
 
+  /// return the view the the model
   FlightSearchView get view => this.flightSearchView;
 
+  /// updates the view with the current model
   void updateView() {
     this.flightSearchView = FlightSearchView(this);
   }
 
+  /// sets the 'from' property in the query to given value
   set flightFrom(String location) => this.model.from = location;
 
+  /// sets 'to' property in the quey to given value
   set flightTo(String location) => this.model.to = location;
 
+  /// sets the 'departureDate' property to given value
   set departureDate(DateTime date) => this.model.departureDate = date;
 
+  /// sets the 'arrivalData' property to the given value
   set arrivalDate(DateTime date) => this.model.arrivalDate = date;
 
+  /// sets the 'isOneWay' property to the given value
   set isOneWay(bool value) => this.model.isOneWay = value;
 
+  /// sets the 'nonStopFlightsOnly' property to the given value
   set nonStopFlightsOnly(bool value) => this.model.nonStopFlightsOnly = value;
 
+  /// returns the origin of the flight
   String get flightFrom => this.model.from;
 
+  /// returns the the destination of the flight
   String get flightTo => this.model.to;
 
+  /// returns the date of departure of the flight
   DateTime get departureDate => this.model.departureDate;
 
+  /// returns the date of arrival of the flight
   DateTime get arrivalDate => this.model.arrivalDate;
 
+  /// returns true if the search is for one-way flights
   bool get isOneWay => this.model.isOneWay;
 
+  /// returns true if the search is for non-stop flights
   bool get nonStopFlightsOnly => this.model.nonStopFlightsOnly;
 
+  /// prints the user serach input entered in the form
+  /// meant for debuging purposes only
   void printUserInput() {
     print("From: ${this.flightFrom}");
     print("To: ${this.flightTo}");
@@ -59,25 +78,31 @@ class FlightSearchController {
     print("Non Stop: ${this.nonStopFlightsOnly}");
   }
 
+  /// analyzes the submitted search query;
   void analyzeSearchQuery() {
     // TODO
   }
 }
 
+/// a view class for [FlightSearchQuery]
 class FlightSearchView {
   FlightSearchFormWidget widget;
   FlightSearchController controller;
 
+  /// a constructor for the class.
+  /// the constructor recieves the controller containing the model
   FlightSearchView(FlightSearchController controller) {
     this.widget = FlightSearchFormWidget(controller);
     this.controller = controller;
   }
 
+  /// returns the visual representatioj of the search form
   Widget render() {
     return this.widget;
   }
 }
 
+/// a widget class for the [FlightSearchQuery]
 class FlightSearchFormWidget extends StatefulWidget {
   final FlightSearchController controller;
 
@@ -87,6 +112,7 @@ class FlightSearchFormWidget extends StatefulWidget {
   _FlightSearchFormWidgetState createState() => _FlightSearchFormWidgetState();
 }
 
+/// a state class for the [FlightSearchFormWidget]
 class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
   final _fromController = TextEditingController();
   final _toController = TextEditingController();
@@ -125,12 +151,14 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns the a widget for the headline of the form
   Widget searchHeadline() {
     return ListTile(
         title:
             Text("Where do you want to go?", style: TextStyle(fontSize: 24)));
   }
 
+  /// returns a text field for the origin of the flight
   Widget fromTextField() {
     return ListTile(
       title: TextField(
@@ -150,6 +178,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a text field for the destinatio of the flight
   Widget toTextField() {
     return ListTile(
       title: TextField(
@@ -169,6 +198,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a text field for the deparature date of the flight
   Widget departureAtTextField() {
     return ListTile(
       title: TextField(
@@ -183,6 +213,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a text field for the arrival date fo the flight
   Widget arrivalAtTextFeild() {
     return ListTile(
       title: TextField(
@@ -199,6 +230,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a check-box for search of one-way flights
   Widget oneWayCheckBox() {
     return CheckboxListTile(
       title: const Text("One-Way"),
@@ -212,6 +244,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a check-box for search of non-stop flights
   Widget nonStopCheckBox() {
     return CheckboxListTile(
       title: const Text("Non-Stop flights only"),
@@ -224,6 +257,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// return the 'search flights' button
   Widget searchButton() {
     return ListTile(
       title: RaisedButton(
@@ -241,14 +275,17 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a decoration for the 'from' text-field
   InputDecoration fromDecoration() {
     return InputDecoration(labelText: "From", hintText: "e.g. New York");
   }
 
+  /// returns a decoration for the 'to' text-field
   InputDecoration toDecoration() {
     return InputDecoration(labelText: "To", hintText: "e.g. Rome");
   }
 
+  /// returns a decoration for 'depatarture date' text-field
   InputDecoration departureAtDecoration() {
     return InputDecoration(
       labelText: "Departure At",
@@ -263,6 +300,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a decoration for 'arrival date' text-field
   InputDecoration arrivalAtDecoration() {
     return InputDecoration(
       labelText: "Arrival At",
@@ -277,10 +315,12 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     );
   }
 
+  /// returns a style fir the text in the form
   TextStyle textStyle() {
     return TextStyle(fontSize: 16);
   }
 
+  // picks the date of the departure
   void pickDepartureDate() {
     Future<DateTime> futureDate = showDatePicker(
         context: context,
@@ -300,6 +340,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     });
   }
 
+  /// picks the date of arrival
   void pickArrivalDate() {
     Future<DateTime> futureDate = showDatePicker(
       context: context,
@@ -320,6 +361,7 @@ class _FlightSearchFormWidgetState extends State<FlightSearchFormWidget> {
     });
   }
 
+  /// return a string representation of a date
   String formattedDate(DateTime date) {
     return "${date.day.toString().padLeft(2, '0')}/${date.month.toString().padLeft(2, '0')}/${date.year.toString().padLeft(2, '0')}";
   }
