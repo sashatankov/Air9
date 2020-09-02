4b6301bfd772bd2539943d37ce4dd1473f2ee87f
sashatankov <isasha96@gmail.com>
2020-09-02 17:23:52 +0300

    documentation added to the 'search results screen' module

--- lib/src/search_results_screen.dart
+++ lib/src/search_results_screen.dart
@@ -5,10 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:Air9/src/search_results_data.dart' show fetchFlights;
 import 'package:Air9/src/flight_search.dart' show FlightSearchQuery;
 
+/// a class representing a single search result ofr a particular flight
 class FlightSearchResult {
   Flight flight;
   String price;
 
+  /// a constructor for the class. The constructor recieves
+  /// the various paraments related to the details of the flight
+  /// e.g origin, destination, date, price, etc.
   FlightSearchResult(
       String flightNumber,
       String departureCity,
@@ -24,26 +28,39 @@ class FlightSearchResult {
     this.price = price;
   }
 
+  /// factory constructor that builds the object based on the data
+  /// passed as a JSON
   factory FlightSearchResult.fromJSON(Map<String, dynamic> json) {
     return FlightSearchResult(); // TODO: to parse the json
   }
 }
 
+/// a class represents a collection of [FlightSearchResult] objects
 class FlightSearchResults {
   List<FlightSearchResult> searchResults;
 
+  /// a constructor for the class. creates an empty collection of search
+  /// results.
   FlightSearchResults() {
     this.searchResults = List<FlightSearchResult>();
   }
 
-  factory FlightSearchResults.fromJSON(List<Map<String, dynamic>> json) {}
+  /// factory method that build a collection of [FlightSearchResult] objects
+  /// from a JSON
+  factory FlightSearchResults.fromJSON(List<Map<String, dynamic>> json) {
+    // TODO:
+  }
 
+  /// return the size of the collection
   int get size => this.searchResults.length;
 }
 
+/// a widget class the represents the visual representation of the
+/// a single [FlightSearchResult]
 class FlightSearchResultWidget extends StatefulWidget {
   final FlightSearchResult model;
 
+  /// a constructor for the class
   FlightSearchResultWidget(this.model);
 
   @override
@@ -51,27 +68,37 @@ class FlightSearchResultWidget extends StatefulWidget {
       _FlightSearchResultWidgetState();
 }
 
+/// a state class for the [FlightSearchResult] widget
 class _FlightSearchResultWidgetState extends State<FlightSearchResultWidget> {
   @override
   Widget build(BuildContext context) {
     return Card();
   }
 
+  /// returns the data about the search result as a [FlightSearchResult] a.k.a 
+  /// the model og the widget
   FlightSearchResult get model => this.widget.model;
+
+  /// return the data about the flight as a [Flight]
   Flight get flight => this.model.flight;
 }
 
+/// a widget class representing the the visual representation
+/// of a list of search results
 class FlightSearchResultsWidget extends StatefulWidget {
   final String title;
   final FlightSearchResults model;
 
+  /// a constructor for the class
   FlightSearchResultsWidget(this.model, {Key key, this.title})
       : super(key: key);
+  
   @override
   _FlightSearchResultsWidgetState createState() =>
       _FlightSearchResultsWidgetState();
 }
 
+/// a state class for the [FlightSearchResultsWidget]
 class _FlightSearchResultsWidgetState extends State<FlightSearchResultsWidget> {
   @override
   Widget build(BuildContext context) {
@@ -82,20 +109,27 @@ class _FlightSearchResultsWidgetState extends State<FlightSearchResultsWidget> {
       },
     );
   }
-
+  /// returns the data about the search result
   FlightSearchResults get model => this.widget.model;
+
+  /// returns a list of search results
   List<FlightSearchResult> get searchResults => this.model.searchResults;
 }
 
+
+/// a widget class representing the entire search results screen
 class FlightSearchResultsScreenWidget extends StatefulWidget {
   final FlightSearchQuery query;
 
+  /// a constructor for the class
+  /// the constructor recieves a [FlightSearchQuery] as a parameter
   FlightSearchResultsScreenWidget(this.query);
   @override
   _FlightSearchResultsScreenWidgetState createState() =>
       _FlightSearchResultsScreenWidgetState();
 }
 
+/// a state class for the [FlightSearchREsultsScreenWidget]
 class _FlightSearchResultsScreenWidgetState
     extends State<FlightSearchResultsScreenWidget> {
   Future<FlightSearchResults> _futureSearchResultFlights;
@@ -117,6 +151,7 @@ class _FlightSearchResultsScreenWidgetState
     );
   }
 
+  /// returns the body of the screen
   Widget getScreenBody() {
     return FutureBuilder<FlightSearchResults>(
       future: this._futureSearchResultFlights,
