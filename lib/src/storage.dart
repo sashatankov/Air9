ed78c9a308e48a2b2dc9f257dd6b0cc5f894915d
sashatankov <isasha96@gmail.com>
2020-10-08 21:37:11 +0300

    cache works now

--- lib/src/storage.dart
+++ lib/src/storage.dart
@@ -7,7 +7,7 @@ class IataCodesStorage {
   Map<String, dynamic> cache;
 
   IataCodesStorage() {
-    cache = Map<String, dynamic>();
+    this.cache = Map<String, dynamic>();
   }
 
   Future<String> get _localPath async {
@@ -21,106 +21,84 @@ class IataCodesStorage {
     return File('codes.json');
   }
 
-  Future<String> getCityName(String cityCode) async {
+  Future<Map<String, dynamic>> loadCacheFromDisk() async {
     try {
-      final file = await this._localFile;
+      Map<String, dynamic> codes;
+      var file = await this._localFile;
+      if (await file.exists() == false) {
+        file = await file.create(recursive: true);
+      }
       String content = await file.readAsString();
-      Map<String, dynamic> codes = json.decode(content);
-      if (codes.containsKey(cityCode)) {
-        return codes[cityCode];
+      if (content.isNotEmpty) {
+        codes = json.decode(content);
+      } else {
+        codes = Map<String, dynamic>();
       }
 
-      throw "Illegal City Code";
+      return codes;
     } catch (e) {
       throw "Unexpected Error Occured";
     }
+  }
+
+  Future<String> getCityName(String cityCode) async {
+    if (this.cache.isEmpty) {
+      var cacheFromDisk = await loadCacheFromDisk();
+      this.cache.addAll(cacheFromDisk);
+    }
+    if (this.cache.containsKey(cityCode)) {
+      return this.cache[cityCode];
+    }
+
+    throw "Illegal City Code";
   }
 
   Future<String> getAirportName(String airportCode) async {
-    try {
-      final file = await this._localFile;
-      String content = await file.readAsString();
-      Map<String, dynamic> codes = json.decode(content);
-      if (codes.containsKey(airportCode)) {
-        return codes[airportCode];
-      }
-
-      throw "Illegal Airport Code";
-    } catch (e) {
-      throw "Unexpected Error Occured";
+    if (this.cache.isEmpty) {
+      var cacheFromDisk = await loadCacheFromDisk();
+      this.cache.addAll(cacheFromDisk);
     }
+    if (this.cache.containsKey(airportCode)) {
+      return this.cache[airportCode];
+    }
+
+    throw "Illegal City Code";
   }
 
   Future<String> getCountryName(String countryCode) async {
-    try {
-      final file = await this._localFile;
-      String content = await file.readAsString();
-      Map<String, dynamic> codes = json.decode(content);
-      if (codes.containsKey(countryCode)) {
-        return codes[countryCode];
-      }
-
-      throw "Illegal Airport Code";
-    } catch (e) {
-      throw "Unexpected Error Occured";
+    if (this.cache.isEmpty) {
+      var cacheFromDisk = await loadCacheFromDisk();
+      this.cache.addAll(cacheFromDisk);
     }
+    if (this.cache.containsKey(countryCode)) {
+      return this.cache[countryCode];
+    }
+
+    throw "Illegal City Code";
   }
 
   void writeCityName(String cityCode, String cityName) async {
-    try {
-      Map<String, dynamic> codes;
-      var file = await _localFile;
-      if (await file.exists() == false) {
-        file = await file.create(recursive: true);
-      }
-      String content = await file.readAsString();
-      if (content.isNotEmpty) {
-        codes = json.decode(content);
-        this.cache.addAll(codes);
-      }
-      this.cache[cityCode] = cityName;
-
-    } catch (e) {
-      throw "Unexpected Error occured";
+    if (this.cache.isEmpty) {
+      var cacheFromDisk = await loadCacheFromDisk();
+      this.cache.addAll(cacheFromDisk);
     }
+    this.cache[cityCode] = cityName;
   }
 
   void writeAirportName(String airportCode, String airportName) async {
-    try {
-      Map<String, dynamic> codes;
-      var file = await _localFile;
-      if (await file.exists() == false) {
-        file = await file.create(recursive: true);
-      }
-      String content = await file.readAsString();
-      if (content.isNotEmpty) {
-        codes = json.decode(content);
-        this.cache.addAll(codes);
-      }
-      this.cache[airportCode] = airportName;
-
-    } catch (e) {
-      throw "Unexpected Error occured";
+    if (this.cache.isEmpty) {
+      var cacheFromDisk = await loadCacheFromDisk();
+      this.cache.addAll(cacheFromDisk);
     }
+    this.cache[airportCode] = airportName;
   }
 
   void writeCountryName(String countryCode, String countryName) async {
-    try {
-      Map<String, dynamic> codes;
-      var file = await _localFile;
-      if (await file.exists() == false) {
-        file = await file.create(recursive: true);
-      }
-      String content = await file.readAsString();
-      if (content.isNotEmpty) {
-        codes = json.decode(content);
-        this.cache.addAll(codes);
-      }
-      this.cache[countryCode] = countryName;
-
-    } catch (e) {
-      throw "Unexpected Error occured";
+    if (this.cache.isEmpty) {
+      var cacheFromDisk = await loadCacheFromDisk();
+      this.cache.addAll(cacheFromDisk);
     }
+    this.cache[countryCode] = countryName;
   }
 
   void dispose() async {
